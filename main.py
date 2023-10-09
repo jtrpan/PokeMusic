f8bf38b44372c2ab56c3901685bb70b3d42c619e
James Pan <jtrpan@gmail.com>
2023-10-09 13:28:45 -0400

    Support for macOS

--- main.py
+++ main.py
@@ -1,13 +1,56 @@
 from PIL import ImageGrab
-from random import randint
 import numpy as np
 import cv2
 import pytesseract
-import winsound
+import platform
+from random import choice
+from screeninfo import get_monitors
 
-# pytesseract.pytesseract.tesseract_cmd z = r'D:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-# pytesseract.pytesseract.tesseract_cmd = r'/Users/jtrpan/Games/Nintendo/PokeMusic/Tesseract-OCR'
-pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
+monitor = get_monitors()[0]  # Get the primary monitor
+screen_width = monitor.width
+screen_height = monitor.height
+
+# Map each gameplay situation to its corresponding music files
+music_files = {
+    "Title Screen": [f"music/title{i}.wav" for i in range(1, 7)],
+    "Open World (Exploration)": [f"music/city{i}.wav" for i in range(1, 16)] + [f"music/route{i}.wav" for i in
+                                                                                range(1, 13)],
+    "Wild Pokemon Battle": [f"music/wild{i}.wav" for i in range(1, 5)],
+    "Trainer Battle": [f"music/trainer{i}.wav" for i in range(1, 5)],
+    "Rival Battle": [f"music/rival{i}.wav" for i in range(1, 4)],
+    "Gym Leader Battle": [f"music/gym{i}.wav" for i in range(1, 8)],
+    "Elite Four Battle": [f"music/elite{i}.wav" for i in range(1, 5)],
+    "Champion Battle": [f"music/champ{i}.wav" for i in range(1, 4)],
+    "Organization (Rocket/Galactic) Grunt Battle": [],  # Add files when available
+    "Organization Commander Battle": [f"music/commander{i}.wav" for i in range(1, 2)],
+    "Organization Boss Battle": [f"music/boss{i}.wav" for i in range(1, 2)],
+    "Legendary Pokemon Battle": ["music/zinnia.wav"],  # Add more files if available
+}
+
+
+def get_platform():
+    if platform.system() == "Windows":
+        # Windows-specific code
+        # pytesseract.pytesseract.tesseract_cmd z = r'D:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+        # pytesseract.pytesseract.tesseract_cmd = r'/Users/jtrpan/Games/Nintendo/PokeMusic/Tesseract-OCR'
+        pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
+    elif platform.system() == "Darwin":
+        # macOS-specific code
+        # pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
+        pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
+
+
+def play_music(situation):
+    # Randomly select a music file from the corresponding list
+    sound_file = choice(music_files[situation])
+    if platform.system() == "Windows":
+        # Windows-specific code
+        import winsound
+        winsound.PlaySound(sound_file, winsound.SND_LOOP + winsound.SND_ASYNC)
+    elif platform.system() == "Darwin":
+        # macOS-specific code
+        import subprocess
+        subprocess.Popen(["afplay", sound_file])
 
 
 def roi(img, vertices):
@@ -20,265 +63,65 @@ def roi(img, vertices):
     return masked
 
 
-def identify(text, situation):
-    if (situation == 0):
-        if ("What will" in text) or ("do?" in text):
-            situation = 5
+def identify(text):
+    situations = {
+        "Wild Pokemon Battle": ["A wild", "appeared"],
+        "Trainer Battle": ["You are challenged", "are challenged by", "challenged by"],
+        "Gym Leader Battle": ["Leader", "Boss", "Gym", "Champion", "Elite", "Admin"],
+        "Rival Battle": ["Rival"],
+        "Open World (Exploration)": ["Save your", "your game", "game with", "your progress", "A device", "device that",
+                                     "that records", "upon meeting", "meeting or", "Check and", "and organize",
+                                     "traveling with", "with you", "Equipped with", "with pockets", "pockets for",
+                                     "you bought", "bought, received", "Check your", "your money", "Got away",
+                                     "away safely"]
+    }
 
-    if ("A wild" in text) or ("appeared" in text):
-        situation = 1
-    elif ("You are" in text) or ("are challenged" in text) or ("challenged by" in text):
-        if ("Leader" in text) or ("Boss" in text) or ("Gym" in text) or ("Champion" in text) or ("Elite" in text) or (
-                "Admin" in text):
-            situation = 2
-        elif ("Rival" in text):
-            situation = 3
-        else:
-            situation = 4
-    elif ("Save your" in text) or \
-            ("your game" in text) or \
-            ("game with" in text) or \
-            ("your progress" in text) or \
-            ("A device" in text) or \
-            ("device that" in text) or \
-            ("that records" in text) or \
-            ("upon meeting" in text) or \
-            ("meeting or" in text) or \
-            ("Check and" in text) or \
-            ("and organize" in text) or \
-            ("traveling with" in text) or \
-            ("with you" in text) or \
-            ("Equipped with" in text) or \
-            ("with pockets" in text) or \
-            ("pockets for" in text) or \
-            ("you bought" in text) or \
-            ("bought, received" in text) or \
-            ("Check your" in text) or \
-            ("your money" in text) or \
-            ("Got away" in text) or \
-            ("away safely" in text):
-        situation = 0
+    for situation, keywords in situations.items():
+        if any(keyword in text for keyword in keywords):
+            return situation
 
-    return situation
+    return None  # Return None if no situation is identified
 
 
 def musicPlayer(situation, keepPlaying):
+    if situation is None:
+        return
     if (keepPlaying):
         return
     else:
-        if (situation == 0):
-            picker = randint(1, 28)
-            if picker == 1:
-                winsound.PlaySound(r'music/city1.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 2:
-                winsound.PlaySound(r'music/city2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 3:
-                winsound.PlaySound(r'music/city2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 4:
-                winsound.PlaySound(r'music/city3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 5:
-                winsound.PlaySound(r'music/city4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 6:
-                winsound.PlaySound(r'music/city5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 7:
-                winsound.PlaySound(r'music/city6.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 8:
-                winsound.PlaySound(r'music/city7.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 9:
-                winsound.PlaySound(r'music/city8.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 10:
-                winsound.PlaySound(r'music/city9.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 11:
-                winsound.PlaySound(r'music/city10.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 12:
-                winsound.PlaySound(r'music/city11.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 13:
-                winsound.PlaySound(r'music/city12.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 14:
-                winsound.PlaySound(r'music/city13.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 15:
-                winsound.PlaySound(r'music/city14.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 16:
-                winsound.PlaySound(r'music/city15.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 17:
-                winsound.PlaySound(r'music/route1.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 18:
-                winsound.PlaySound(r'music/route2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 19:
-                winsound.PlaySound(r'music/route3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 20:
-                winsound.PlaySound(r'music/route4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 21:
-                winsound.PlaySound(r'music/route5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 22:
-                winsound.PlaySound(r'music/route6.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 23:
-                winsound.PlaySound(r'music/route7.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 24:
-                winsound.PlaySound(r'music/route8.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 25:
-                winsound.PlaySound(r'music/route9.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 26:
-                winsound.PlaySound(r'music/route10.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 27:
-                winsound.PlaySound(r'music/route11.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 28:
-                winsound.PlaySound(r'music/route12.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-
-        elif (situation == 1):
-            picker = randint(1, 4)
-            if picker == 1:
-                winsound.PlaySound(r'music/wildG3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 2:
-                winsound.PlaySound(r'music/wildG4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 3:
-                winsound.PlaySound(r'music/wildG4.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 4:
-                winsound.PlaySound(r'music/wildG5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-
-        elif (situation == 2):
-            picker = randint(1, 17)
-            if picker == 1:
-                winsound.PlaySound(r'music/champG3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 2:
-                winsound.PlaySound(r'music/champG4.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 3:
-                winsound.PlaySound(r'music/champG4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 4:
-                winsound.PlaySound(r'music/eliteG3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 5:
-                winsound.PlaySound(r'music/eliteG5.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 6:
-                winsound.PlaySound(r'music/eliteG5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 7:
-                winsound.PlaySound(r'music/gymG3.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 8:
-                winsound.PlaySound(r'music/gymG3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 9:
-                winsound.PlaySound(r'music/gymG3.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 10:
-                winsound.PlaySound(r'music/gymG4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 11:
-                winsound.PlaySound(r'music/gymG4.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 12:
-                winsound.PlaySound(r'music/gymG5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 13:
-                winsound.PlaySound(r'music/gymG5.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 14:
-                winsound.PlaySound(r'music/gymG8.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 15:
-                winsound.PlaySound(r'music/marnie.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 16:
-                winsound.PlaySound(r'music/zinnia.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 17:
-                winsound.PlaySound(r'music/zinnia2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-
-        elif (situation == 3):
-            picker = randint(1, 3)
-            if picker == 1:
-                winsound.PlaySound(r'music/rivalG3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 2:
-                winsound.PlaySound(r'music/rivalG4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 3:
-                winsound.PlaySound(r'music/rivalG5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-
-        elif (situation == 4):
-            picker = randint(1, 4)
-            if picker == 1:
-                winsound.PlaySound(r'music/trainerG3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 2:
-                winsound.PlaySound(r'music/trainerG4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 3:
-                winsound.PlaySound(r'music/trainerG4.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 4:
-                winsound.PlaySound(r'music/trainerG5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-
-        elif (situation == 5):
-            picker = randint(1, 26)
-            if picker == 1:
-                winsound.PlaySound(r'music/champG3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 2:
-                winsound.PlaySound(r'music/champG4.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 3:
-                winsound.PlaySound(r'music/champG4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 4:
-                winsound.PlaySound(r'music/eliteG3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 5:
-                winsound.PlaySound(r'music/eliteG5.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 6:
-                winsound.PlaySound(r'music/eliteG5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 7:
-                winsound.PlaySound(r'music/gymG3.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 8:
-                winsound.PlaySound(r'music/gymG3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 9:
-                winsound.PlaySound(r'music/gymG3.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 10:
-                winsound.PlaySound(r'music/gymG4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 11:
-                winsound.PlaySound(r'music/gymG4.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 12:
-                winsound.PlaySound(r'music/gymG5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 13:
-                winsound.PlaySound(r'music/gymG5.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 14:
-                winsound.PlaySound(r'music/gymG8.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 15:
-                winsound.PlaySound(r'music/marnie.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 16:
-                winsound.PlaySound(r'music/zinnia.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 17:
-                winsound.PlaySound(r'music/zinnia2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 18:
-                winsound.PlaySound(r'music/rivalG3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 19:
-                winsound.PlaySound(r'music/rivalG4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 20:
-                winsound.PlaySound(r'music/rivalG5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 21:
-                winsound.PlaySound(r'music/trainerG3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 22:
-                winsound.PlaySound(r'music/trainerG4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 23:
-                winsound.PlaySound(r'music/trainerG4.2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 24:
-                winsound.PlaySound(r'music/trainerG5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 25:
-                winsound.PlaySound(r'music/bossG4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 26:
-                winsound.PlaySound(r'music/commanderG4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-
-        elif (situation == 6):
-            picker = randint(1, 6)
-            if picker == 1:
-                winsound.PlaySound(r'music/title1.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 2:
-                winsound.PlaySound(r'music/title2.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 3:
-                winsound.PlaySound(r'music/title3.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 4:
-                winsound.PlaySound(r'music/title4.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 5:
-                winsound.PlaySound(r'music/title5.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-            elif picker == 6:
-                winsound.PlaySound(r'music/title6.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
-
+        play_music(situation)
     return
+
+
+def get_player_mode():
+    while True:
+        print('Enter 0 for text detection, or 1 for manual control')
+        try:
+            playerMode = int(input())
+            if playerMode in [0, 1]:
+                return playerMode
+            else:
+                print("Invalid input. Please enter 0 or 1.")
+        except ValueError:
+            print("Please enter a valid number.")
 
 
 def main():
     situation = 6
     lastSit = 7
     firstEntry = True
-    print('Enter 0 for text detection, or other for manual control')
-    playerMode = int(input())
+
+    get_platform()
+
+    playerMode = get_player_mode()
     if playerMode == 0:
         print('Text detection selected.')
     else:
         print('Manual control selected.')
     while True:
         if playerMode == 0:
-            img = ImageGrab.grab(bbox=(0, 0, 1200, 850))  # bbox specifies specific region (bbox= x,y,width,height)
+            auto_bbox = (0, screen_height // 2, screen_width, screen_height)
+            img = ImageGrab.grab(bbox=auto_bbox)  # bbox specifies specific region (bbox= x,y,width,height)
             img_np = np.array(img)
             frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
             processed_img = cv2.Canny(frame, threshold1=100, threshold2=350)
@@ -290,7 +133,7 @@ def main():
             config = '-l eng --oem 1 --psm 3'
             # PyTesseract
             myText = pytesseract.image_to_string(processed_img, config=config)
-            situation = identify(myText, situation)
+            situation = identify(myText)
             if situation != lastSit:
                 lastSit = situation
                 musicPlayer(situation, False)
